205bac627cbacdd65aedf3d2691ff7f6941ba522
ChxDesk <chackop@gmail.com>
2021-01-12 23:30:58 +0000

    terminal command exec

--- app4/exec.js
+++ app4/exec.js
@@ -1,5 +1,9 @@
 const cp = require("child_process");
 
+// cp.exec("ls", (err, data, stderr) => {
+//   console.log(data);
+// });
+
 cp.exec("node readStream", (err, data, stderr) => {
   console.log(data);
 });
